d485c1d460db980e2f1e4baaa2b4536734359836
shimanche <shimanche.sute@gmail.com>
2024-04-24 07:34:58 +0900

    first-commit

--- react-csvupload/src/App.tsx
+++ react-csvupload/src/App.tsx
@@ -6,6 +6,8 @@ import SimpleSortableItem from './components/SimpleSortableItem';
 import ItemForNameCompany from './components/ItemForNameCompany';
 import { downloadCSV } from './utils/csvDownloader';
 import { doubleList } from './utils/doubleList';
+import DoubleContext from './components/doubleContext';
+import { CheckboxGroup } from './components/checkboxGroup';
 
 interface DataItem {
   No: number;
@@ -88,7 +90,7 @@ const App: React.FC = () => {
       setResponseDataDouble(doubleList(responseData));
     }
   }, [responseData]);
-  console.log('responseDataDouble', responseDataDouble);
+  // console.log('responseDataDouble', responseDataDouble);
 
   const getMsg = async () => {
     try {
@@ -96,6 +98,76 @@ const App: React.FC = () => {
       console.log('response:', response);
     } catch (error) {
       console.log(error);
+    }
+  };
+
+  // ーーーーーcheckboxーーーーーー
+  const [selectedGroups, setSelectedGroups] = useState<string[]>([]);
+  const [group1, setGroup1] = useState<any[] >([]); // グループ1のデータ
+  const [group2, setGroup2] = useState<any[]>([]); // グループ2のデータ
+
+  const handleCheckboxChange = (selectedGroups: string[]) => {
+    setSelectedGroups(selectedGroups);
+  };
+  const handleMoveToGroup = (
+    id: number,
+    sourceGroup: DataItem[],
+    setSourceGroup: React.Dispatch<React.SetStateAction<any[]>>,
+    destinationGroup: DataItem[],
+    setDestinationGroup: React.Dispatch<
+      React.SetStateAction<DataItem[] | null>
+    >,
+  ) => {
+    // idに一致するデータを移動元から取得
+    const dataToMove = sourceGroup.find((data) => data.id === id);
+    if (dataToMove) {
+      // 移動元から該当データを削除
+      setSourceGroup(sourceGroup.filter((data) => data.id !== id));
+      // 移動先にデータを追加
+      setDestinationGroup([...destinationGroup, dataToMove]);
+    }
+  };
+  const handleDelete = (
+    id: number,
+    sourceGroup: DataItem[] | null,
+    setSourceGroup: React.Dispatch<React.SetStateAction<DataItem[] | null>>,
+  ) => {
+    setSourceGroup(sourceGroup?.filter((data) => data.id !== id) || null);
+  };
+  const handleDeleteFromGroup = (
+    id: number,
+    sourceGroup: DataItem[] | null,
+    setSourceGroup: React.Dispatch<React.SetStateAction<DataItem[]>>,
+  ) => {
+    setSourceGroup(sourceGroup!.filter((data) => data.id !== id));
+  };
+
+  const handleMoveToGroup1 = (id: number) => {
+    // idに一致するデータをnameListから取得
+    if (responseData) {
+      const dataToMove = responseData.find((data) => data.id === id);
+      if (dataToMove) {
+        // responseDataから該当データを削除
+        setResponseData(responseData.filter((data) => data.id !== id));
+        // グループ1にデータを追加
+        setGroup1([...group1, dataToMove]);
+      }
+    } else {
+      console.log('responseData is Null ! ');
+    }
+  };
+  const handleMoveToGroup2 = (id: number) => {
+    // idに一致するデータをnameListから取得
+    if (responseData) {
+      const dataToMove = responseData.find((data) => data.id === id);
+      if (dataToMove) {
+        // responseDataから該当データを削除
+        setResponseData(responseData.filter((data) => data.id !== id));
+        // グループ1にデータを追加
+        setGroup2([...group2, dataToMove]);
+      }
+    } else {
+      console.log('responseData is Null ! ');
     }
   };
 
@@ -108,6 +180,167 @@ const App: React.FC = () => {
       <button onClick={() => downloadCSV(responseDataDouble)}>
         Download CSV double
       </button>
+      {/* checkbox */}
+      <div>
+        <h1>Choose Groups</h1>
+        <CheckboxGroup
+          selectedGroups={selectedGroups}
+          onChange={handleCheckboxChange}
+        />
+      </div>
+      <div style={{ display: 'flex' }}>
+        <div style={{ padding: '20px' }}>
+          {/* nameList */}
+          <h2>name List:</h2>
+          {responseData?.map((data, index) => (
+            <div
+              style={{
+                display: 'flex',
+                alignItems: 'center',
+                marginBottom: '5px', // アイテム間のマージンを追加
+                padding: '10px',
+                border: '1px solid #ccc',
+                borderRadius: '5px',
+                transition: 'background-color 0.3s ease',
+              }}
+            >
+              <div style={{ marginRight: '10px', fontWeight: 'bold' }}>
+                {index + 1}
+              </div>
+              <ItemForNameCompany key={data.id} item={data} />
+              {selectedGroups.includes('group1') && ( // グループ1が選択されている場合にのみ移動ボタンを表示
+                <button
+                  onClick={() => handleMoveToGroup1(data.id)}
+                  style={{ marginLeft: '10px' }}
+                >
+                  Move to Group 1
+                </button>
+              )}
+              {selectedGroups.includes('group2') && ( // グループ1が選択されている場合にのみ移動ボタンを表示
+                <button
+                  onClick={() => handleMoveToGroup2(data.id)}
+                  style={{ marginLeft: '10px' }}
+                >
+                  Move to Group 2
+                </button>
+              )}
+              <button
+                onClick={() =>
+                  handleDelete(data.id, responseData, setResponseData)
+                }
+                style={{ marginLeft: '10px' }}
+              >
+                delete
+              </button>
+            </div>
+          ))}
+        </div>
+        {/* Group1 */}
+        <div style={{ padding: '20px' }}>
+          <h2>Group1:</h2>
+          {group1?.map((data, index) => (
+            <div
+              style={{
+                display: 'flex',
+                alignItems: 'center',
+                marginBottom: '5px', // アイテム間のマージンを追加
+                padding: '10px',
+                border: '1px solid #ccc',
+                borderRadius: '5px',
+                transition: 'background-color 0.3s ease',
+              }}
+            >
+              <div style={{ marginRight: '10px', fontWeight: 'bold' }}>
+                {index + 1}
+              </div>
+              <ItemForNameCompany key={data.id} item={data} />
+              {selectedGroups.includes('group1') && ( // グループ1が選択されている場合にのみ移動ボタンを表示
+                <button
+                  onClick={() =>
+                    handleMoveToGroup(
+                      data.id,
+                      group1,
+                      setGroup1,
+                      responseData || [],
+                      setResponseData,
+                    )
+                  }
+                  style={{ marginLeft: '10px' }}
+                >
+                  back
+                </button>
+              )}
+                            <button
+                onClick={() =>
+                  handleDeleteFromGroup(data.id, group1, setGroup1)
+                }
+                style={{ marginLeft: '10px' }}
+              >
+                delete
+              </button>
+            </div>
+          ))}
+        </div>
+        {/* Group2 */}
+        <div style={{ padding: '20px' }}>
+          <h2>Group2:</h2>
+          {group2?.map((data, index) => (
+            <div
+              style={{
+                display: 'flex',
+                alignItems: 'center',
+                marginBottom: '5px', // アイテム間のマージンを追加
+                padding: '10px',
+                border: '1px solid #ccc',
+                borderRadius: '5px',
+                transition: 'background-color 0.3s ease',
+              }}
+            >
+              <div style={{ marginRight: '10px', fontWeight: 'bold' }}>
+                {index + 1}
+              </div>
+              <ItemForNameCompany key={data.id} item={data} />
+              {selectedGroups.includes('group2') && ( // グループ1が選択されている場合にのみ移動ボタンを表示
+                <button
+                  onClick={() =>
+                    handleMoveToGroup(
+                      data.id,
+                      group2,
+                      setGroup2,
+                      responseData || [],
+                      setResponseData,
+                    )
+                  }
+                  style={{ marginLeft: '10px' }}
+                >
+                  back
+                </button>
+              )}
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <hr
+        style={{
+          border: 'none',
+          height: '2px', // 線の太さ
+          backgroundColor: '#333', // 線の色
+          margin: '20px 0', // 上下の余白
+        }}
+      />
+
+      {/* 受け取ったデータを表示 */}
+      <h2>Double Data:</h2>
+      <DoubleContext />
+      <hr
+        style={{
+          border: 'none',
+          height: '2px', // 線の太さ
+          backgroundColor: '#333', // 線の色
+          margin: '20px 0', // 上下の余白
+        }}
+      />
       {/* 受け取ったデータを表示 */}
 
       <h2>Response Data:</h2>
